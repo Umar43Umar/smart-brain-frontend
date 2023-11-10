3eb4bc5bd070c03074e9e3cdccd266f1b8989800
Umar43Umar <umar43umar@gmail.com>
2023-11-10 11:59:34 +0500

    new

---
+++ src/components/Signin/Signin.js
@@ -0,0 +1,87 @@
+import React from 'react';
+
+class Signin extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			signInEmail: '',
+			signInPassword: ''
+		}
+	}
+	onEmailChange = (event) => {
+		this.setState({signInEmail: event.target.value})
+	}
+	onPasswordChange = (event) => {
+		this.setState({signInPassword: event.target.value})
+	}
+	onSubmitSignIn = () => {
+	  const { signInEmail, signInPassword } = this.state;
+
+	  // Validate input fields
+	  if (!signInEmail || !signInPassword) {
+	    console.error("Email and password are required");
+	    return; // Return early if any required field is empty
+	  }
+
+	  fetch('http://localhost:3000/signin', {
+	    method: 'post',
+	    headers: { 'Content-Type': 'application/json' },
+	    body: JSON.stringify({
+	      email: signInEmail,
+	      password: signInPassword,
+	    })
+	  })
+	    .then(response => {
+	      if (response.ok) {
+	        return response.json(); // Parse the response if it's successful
+	      } else {
+	        throw new Error("Sign in failed"); // Handle non-200 status codes
+	      }
+	    })
+	    .then(user => {
+	      if (user.id) {
+	        this.props.loadUser(user);
+	        this.props.onRouteChange('home');
+	      } else {
+	        throw new Error("Invalid response from the server");
+	      }
+	    })
+	    .catch(error => {
+	      console.error('Error:', error);
+	      // Handle any network errors or issues with the fetch request
+	      // You can also update the UI to display an error message to the user
+	    });
+	}
+
+
+	render(){
+		const { onRouteChange } = this.props;
+		return(
+			<article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-1 mw6 shadow-5 center">
+				<main className="pa4 black-80">
+		  			<div className="measure">
+		    			<fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+		      				<legend className="f1 fw6 ph0 mh0">Sign In</legend>
+		      				<div className="mt3">
+		        				<label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+		        				<input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" onChange = {this.onEmailChange}/>
+		      				</div>
+		      				<div className="mv3">
+		        				<label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+		        				<input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" onChange={this.onPasswordChange}/>
+		      				</div>
+		    			</fieldset>
+		    			<div className="">
+		      				<input onClick={this.onSubmitSignIn} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
+		    			</div>
+		    			<div className="lh-copy mt3">
+		      				<p onClick={() => this.props.onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
+		    			</div>
+		  			</div>
+				</main>
+			</article>	
+		);
+	}
+}
+
+export default Signin;
